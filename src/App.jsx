5fa1d49b2ace5901a1b0736af8825005c747ec61
quagtoan0210 <quangtoan2k4@gmail.com>
2025-02-04 17:44:45 +0700

    v42

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ const App = () => {
     }
     SetTodoList([...todoList, newTodo]);
   }
+  const deleteTodo = (id)=>{
+    const newTodo = todoList.filter(item=>item.id !== id);
+    SetTodoList(newTodo);
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
@@ -21,6 +25,7 @@ const App = () => {
       {todoList.length > 0 ?
         <TodoData
           todoList={todoList}
+          deleteTodo={deleteTodo}
         />
         :
         <div className='todo-image'>
