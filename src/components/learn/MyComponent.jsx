0ef40f3bfab07f528134f55827a892af418e45fb
quagtoan0210 <quangtoan2k4@gmail.com>
2025-02-04 14:54:00 +0700

    v31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
 import './style.css';
 const MyComponent = ()=>{
+  const name = "join";//string
+  const age = 20; //number
+  const array = [1,2,3,4,5,6,7,8];
+  const object = {name: "Quang", age: 20};
   return (
     <>
-        <div className="child" style={{fontSize:"10px"}}>Quang Toan</div>
+        <div className="child" style={{fontSize:"30px"}}>Quang {name}</div>
+        <div>age: {age}</div>
         <div>MyComponent</div>
+        <div>{array}</div>
+        <div>{JSON.stringify(object)}</div>
 
     </>
   )
