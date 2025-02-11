0e84454c6045ac85c145b8662ba737572ed8e84c
quagtoan0210 <quangtoan2k4@gmail.com>
2025-02-11 18:41:25 +0700

    v78

--- src/components/user/update.user.modal.jsx
+++ src/components/user/update.user.modal.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { Input, notification, Modal } from "antd";
-import { createUserAPI } from "../../services/api.service";
+import { createUserAPI, updateUserAPI } from "../../services/api.service";
 
 const UpdateUserModal = (props) => {
     const [id, setId] = useState("");
     const [fullName, setFullName] = useState("");
     const [phone, setPhone] = useState("");
 
-    const { isModalUpdateOpen, setIsModalUpdateOpen, dataUpdate, setDataUpdate } = props;
+    const { isModalUpdateOpen, setIsModalUpdateOpen, dataUpdate, setDataUpdate,loadUser } = props;
     useEffect(() => {
         if (dataUpdate) {
             setId(dataUpdate._id);
@@ -18,14 +18,14 @@ const UpdateUserModal = (props) => {
     }, [dataUpdate])
 
     const handleSubmitBtn = async () => {
-        const res = await createUserAPI(fullName, email, password, phone);
+        const res = await updateUserAPI(id,fullName,phone);
         if (res.data) {
             notification.success({
-                message: "Create user",
-                description: "Tạo user thành công"
+                message: "Update user",
+                description: "Cập nhật user thành công"
             })
             resetAndCloseModal();
-            // await loadUser();
+            await loadUser();
 
         } else {
             notification.error({
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -22,7 +22,8 @@ const UserPage = () => {
     return (
         <div style={{ padding: "20px" }}>
             <UserForm loadUser={loadUser} />
-            <UserTable dataUsers={dataUsers} />
+            <UserTable dataUsers={dataUsers}
+                loadUser={loadUser} />
         </div>
     )
 }
