a5388c2072ec504b1a0b399ad5d7f29e9c97911f
quagtoan0210 <quangtoan2k4@gmail.com>
2025-02-11 15:52:16 +0700

    v73

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,39 +1,45 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import axios from "axios";
 import { useState } from "react";
+import { createUserAPI } from "../../services/api.service";
 const UserForm = () => {
-    const [fullName,setFullname] = useState("");
+    const [fullName, setFullname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        if (res.data) {
+            notification.success({
+                message: "create user",
+                description: "User created successfully",
+            })
+        }else {
+            notification.error({
+                message: "create user",
+                description: JSON.stringify(res.message),
+            })
         }
-        axios.post(URL_BACKEND, data)
+
     }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>Full Name</span>
-                    <Input value={fullName} onChange={(e) => { setFullname(e.target.value) }}/>
+                    <Input value={fullName} onChange={(e) => { setFullname(e.target.value) }} />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <Input value={email} onChange={(e) => { setEmail(e.target.value) }} />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                    <Input.Password value={password} onChange={(e) => { setPassword(e.target.value) }} />
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input value={phone} onChange={(e)=>{setPhone(e.target.value)}}/>
+                    <Input value={phone} onChange={(e) => { setPhone(e.target.value) }} />
                 </div>
                 <div>
                     <Button type="primary" onClick={handleClickBtn}> Create User </Button>
