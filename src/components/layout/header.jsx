a8cdb24be8689f297a31a840647ab8e8914b0711
quagtoan0210 <quangtoan2k4@gmail.com>
2025-02-18 14:35:23 +0700

    v93

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,15 +1,15 @@
 import { Link, NavLink } from 'react-router-dom';
 import { Menu } from 'antd';
-import { UsergroupAddOutlined, HomeOutlined, AuditOutlined } from '@ant-design/icons';
+import { UsergroupAddOutlined, HomeOutlined, AuditOutlined, SettingOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 
 const Header = () => {
-    //anchor
     const [current, setCurrent] = useState('');
     const onClick = (e) => {
         console.log('click ', e);
         setCurrent(e.key);
     };
+
     const items = [
         {
             label: <Link to={"/"}>Home</Link>,
@@ -26,7 +26,25 @@ const Header = () => {
             key: 'books',
             icon: <AuditOutlined />,
         },
+
+        {
+            label: 'Cài đặt',
+            key: 'setting',
+            icon: <SettingOutlined />,
+            children: [
+                {
+                    label: <Link to={"/login"}>Đăng nhập</Link>,
+                    key: 'login',
+                },
+                {
+                    label: 'Đăng xuất',
+                    key: 'logout',
+                },
+            ],
+        },
+
     ];
+
     return (
         <Menu
             onClick={onClick}
@@ -36,4 +54,5 @@ const Header = () => {
         />
     )
 }
-export default Header
+
+export default Header;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,4 +1,5 @@
 import axios from './axios.customize';
+
 const createUserAPI = (fullName, email, password, phone) => {
     const URL_BACKEND = "/api/v1/user";
     const data = {
@@ -9,6 +10,7 @@ const createUserAPI = (fullName, email, password, phone) => {
     }
     return axios.post(URL_BACKEND, data);
 }
+
 const updateUserAPI = (_id, fullName, phone) => {
     const URL_BACKEND = "/api/v1/user";
     const data = {
@@ -18,14 +20,17 @@ const updateUserAPI = (_id, fullName, phone) => {
     }
     return axios.put(URL_BACKEND, data);
 }
+
 const fetchAllUserAPI = (current, pageSize) => {
     const URL_BACKEND = `/api/v1/user?current=${current}&pageSize=${pageSize}`;
     return axios.get(URL_BACKEND);
 }
+
 const deleteUserAPI = (id) => {
     const URL_BACKEND = `/api/v1/user/${id}`;//backtick
     return axios.delete(URL_BACKEND);
 }
+
 const handleUploadFile = (file, folder) => {
     const URL_BACKEND = `/api/v1/file/upload`;
     let config = {
@@ -34,10 +39,13 @@ const handleUploadFile = (file, folder) => {
             "Content-Type": "multipart/form-data"
         }
     }
+
     const bodyFormData = new FormData();
     bodyFormData.append("fileImg", file)
+
     return axios.post(URL_BACKEND, bodyFormData, config);
 }
+
 const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
     const URL_BACKEND = "/api/v1/user";
     const data = {
@@ -48,6 +56,7 @@ const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
     }
     return axios.put(URL_BACKEND, data);
 }
+
 const registerUserAPI = (fullName, email, password, phone) => {
     const URL_BACKEND = "/api/v1/user/register";
     const data = {
@@ -58,6 +67,19 @@ const registerUserAPI = (fullName, email, password, phone) => {
     }
     return axios.post(URL_BACKEND, data);
 }
+
+const loginAPI = (email, password) => {
+    const URL_BACKEND = "/api/v1/auth/login";
+    const data = {
+        username: email,
+        password: password,
+        delay: 2000
+    }
+    return axios.post(URL_BACKEND, data);
+}
+
 export {
-    createUserAPI, updateUserAPI, fetchAllUserAPI, deleteUserAPI, handleUploadFile, updateUserAvatarAPI,registerUserAPI
+    createUserAPI, updateUserAPI, fetchAllUserAPI,
+    deleteUserAPI, handleUploadFile, updateUserAvatarAPI,
+    registerUserAPI, loginAPI
 }
